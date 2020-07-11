c5b84573becfe73483fff359e708a13fd167d2fc
Androxium <contact.chengthomas@gmail.com>
2020-07-11 12:53:12 -0400

    added basic create playlist logic

--- src/ui/PlaylistCreator.js
+++ src/ui/PlaylistCreator.js
@@ -1,15 +1,19 @@
 import * as React from 'react';
 import * as CallApi from '../utils/APICalls';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+import './PlaylistCreator.scss';
 
 export default class PlaylistCreator extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {playlistText: ''};
   }
 
-  createPlaylist = () => {
+  createPlaylist = (playlistName) => {
     CallApi.createPlaylist(
-      `Playlist${this.props.playlists.length}`,
+      playlistName,
       this.props.userId
     )
       .then(res => {
@@ -20,7 +24,46 @@ export default class PlaylistCreator extends React.Component {
     alert('create a new playlist');
   };
 
+  handleChange = e => {
+    this.setState({ playlistText: e.target.value });
+  };
+
+  handleOnClick = () => {
+    if (this.state.playlistText === ''){
+      alert('Playlist name must be non-empty');
+      return;
+    }
+    this.createPlaylist(this.state.playlistText);
+    this.setState({playlistText: ''});
+  };
+
   render() {
-    return <div>Working In Progress Put Create Playlist logic here</div>;
+    return (
+      <div className='playlist-creator-container'>
+        <div className='creator-title'>
+          Enter the name of your new playlist
+        </div>
+        <div className='creator-textfield-container'>
+          <div className='creator-text'>
+            <TextField 
+              id="standard-basic"
+              label="Playlist name"
+              fullWidth
+              onChange={this.handleChange}
+              value={this.state.playlistText}
+            />
+          </div>
+          <div className='creator-button'>
+            <Button 
+              variant="contained" 
+              color="primary"
+              onClick={this.handleOnClick}
+            >
+              Create Playlist
+            </Button>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
